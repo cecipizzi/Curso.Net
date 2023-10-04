875d3184167ea83941ffcba84ae07cc871f85f19
cpizzi <cecilia.pizzi@cablasociados.com>
2023-10-04 12:40:10 -0300

    se agrega proyecto

---
+++ MiProyecto-Cubits/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiProyecto_Cubits.Models;
+
+namespace MiProyecto_Cubits.Controllers
+{
+    [Route("Users")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = new UserModel
+            {
+                Name = "Cecilia",
+                LastName = "Pizzi"
+            };
+
+            return View("Index",user); 
+        }   
+    }
+}
---
+++ MiProyecto-Cubits/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MiProyecto_Cubits.Models
+{
+    public class UserModel
+    {
+        public string? Name { get; set; }
+        
+        public string? LastName  { get; set; }   
+    }
+}
